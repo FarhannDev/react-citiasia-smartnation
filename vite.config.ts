07c3e765625aec7ee3fde068b39252f33b8e1f62
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2024-03-15 16:31:09 +0700

    Add service worker

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,63 @@ export default defineConfig({
       mozjpeg: { quality: 80 },
       webp: { quality: 75 },
     }),
-    VitePWA(),
+    VitePWA({
+      manifest: {
+        name: 'Smart Nation',
+        short_name: 'Smart Nation Lite',
+        description:
+          'Citiasia Center for Smart Nation (CCSN) merupakan salah satu sayap strategis dari Citiasia Inc. dalam menyebarkan semangat membangun bangsa menuju Indonesia Smart Nation',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/smartnation/logo-smartnation-id_64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: '/images/smartnation/logo-smartnation-id_192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/images/smartnation/logo-smartnation-id_512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        // Konfigurasi Workbox
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 7 * 24 * 60 * 60, // 7 hari
+              },
+            },
+          },
+          {
+            urlPattern: new RegExp('^http://localhost:4173/'),
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60, // 1 jam
+              },
+            },
+          },
+        ],
+      },
+    }),
     ViteMinifyPlugin({}),
   ],
   resolve: {
